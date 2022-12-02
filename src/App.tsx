7b063cf47e58fe5a3dc1063a76841ac8edf8a2fd
okmtyuta <okmtyuta@gmail.com>
2022-12-02 23:52:43 +0900

    プレーンなエディタ

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Editor } from "./Editor/Editor";
 
 function App() {
   return (
     <div>
-      Hello Lexical
+      <Editor />
     </div>
   );
 }
--- src/Editor/Editor.tsx
+++ src/Editor/Editor.tsx
@@ -4,6 +4,8 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
+import "./editor.css"
+
 const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
   namespace: "MyEditor",
   onError: (error) => console.error(error),
@@ -11,12 +13,14 @@ const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
 
 export const Editor: FC = () => {
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Hello Lexical</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-    </LexicalComposer>
+    <div>
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable className="editor-content-editable" />}
+          placeholder={<div>Hello Lexical</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </LexicalComposer>
+    </div>
   );
 };
